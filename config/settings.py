75b7c8eff249eb137db409f331aa25beadc15dbe
ulianatrachuk <uliana.trachuk.pp.2022@lpnu.ua>
2026-05-21 01:47:59 +0300

    env config

--- config/settings.py
+++ config/settings.py
@@ -27,11 +27,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-y3t8&gla*gkrq1te)7u0f4d&78@=fn0nfw=pnwvn+^-xvg(5j&"
+SECRET_KEY = os.getenv("SECRET_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = [
     "localhost",
