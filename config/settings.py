c3c2e53cf60ad60f8a583627872a290234d1c364
ulianatrachuk <uliana.trachuk.pp.2022@lpnu.ua>
2026-06-08 21:41:10 +0300

    Fix CORS for Flutter Web

--- config/settings.py
+++ config/settings.py
@@ -167,4 +167,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "https://animal-shelter-app-two.vercel.app",
+]
+
 CORS_ALLOW_ALL_ORIGINS = True
